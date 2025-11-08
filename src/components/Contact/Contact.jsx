8d4b22005797584594c966819051018db348f5f9
RAKSHITH PR <codewithrax@gmail.com>
2025-11-08 16:35:14 +0530

    contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,10 +12,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_axbtt7a",  // Replace with your EmailJS Service ID
-        "template_1ziboq3",  // Replace with your EmailJS Template ID
+        "service_j98ijkj",  // Replace with your EmailJS Service ID
+        "template_b408nss",  // Replace with your EmailJS Template ID
         form.current,
-        "Rz7W9pVF0HdDryNNL"  // Replace with your EmailJS Public Key
+        "zdWd0AelnMC8Denab"  // Replace with your EmailJS Public Key
       )
       .then(
         () => {
